02ce729bb0f3667a83295083e6f1f179d54a1f11
Boseong Choi <31615733+cbscsm@users.noreply.github.com>
2022-05-29 20:00:21 +0900

    최보성 2장 제출

---
+++ chapter2/최보성-2장-ecc.py
@@ -0,0 +1,188 @@
+from typing import Optional
+from unittest import TestCase
+
+from typing_extensions import Self
+
+
+class FieldElement:
+    def __init__(self, num: int, prime: int):
+        if not (0 <= num < prime):
+            raise ValueError(f'{num} not in range [0, {prime})')
+        self.num = num
+        self.prime = prime
+
+    def create_in_same_order(self, num: int) -> Self:
+        return self.__class__(num % self.prime, self.prime)
+
+    def check_same_order(self, other: 'FieldElement', op: str):
+        if self.prime != other.prime:
+            raise TypeError(f'Cannot {op} two numbers in different Fields')
+
+    def __repr__(self):
+        return f'FieldElement({self.num}, {self.prime})'
+
+    def __eq__(self, other: object):
+        if isinstance(other, FieldElement):
+            return self.num == other.num and self.prime == other.prime
+        return False
+
+    def __add__(self, other: 'FieldElement') -> Self:
+        if isinstance(other, FieldElement):
+            self.check_same_order(other, 'add')
+            return self.create_in_same_order(self.num + other.num)
+        return NotImplemented
+
+    def __sub__(self, other: 'FieldElement') -> Self:
+        if isinstance(other, FieldElement):
+            self.check_same_order(other, 'subtract')
+            return self.create_in_same_order(self.num - other.num)
+        return NotImplemented
+
+    def __mul__(self, other: 'FieldElement') -> Self:
+        if isinstance(other, FieldElement):
+            self.check_same_order(other, 'multiply')
+            return self.create_in_same_order(self.num * other.num)
+        return NotImplemented
+
+    def __pow__(self, exponent: int) -> Self:
+        if isinstance(exponent, int):
+            n = exponent % (self.prime - 1)
+            return self.create_in_same_order(pow(self.num, n, self.prime))
+        return NotImplemented
+
+    def __truediv__(self, other: object) -> Self:
+        if isinstance(other, FieldElement):
+            self.check_same_order(other, 'divide')
+            return self * other ** -1
+        return NotImplemented
+
+
+class FieldElementTest(TestCase):
+
+    def test_ne(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(2, 31)
+        c = FieldElement(15, 31)
+        self.assertEqual(a, b)
+        self.assertTrue(a != c)
+        self.assertFalse(a != b)
+
+    def test_add(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(15, 31)
+        self.assertEqual(a + b, FieldElement(17, 31))
+        a = FieldElement(17, 31)
+        b = FieldElement(21, 31)
+        self.assertEqual(a + b, FieldElement(7, 31))
+
+    def test_sub(self):
+        a = FieldElement(29, 31)
+        b = FieldElement(4, 31)
+        self.assertEqual(a - b, FieldElement(25, 31))
+        a = FieldElement(15, 31)
+        b = FieldElement(30, 31)
+        self.assertEqual(a - b, FieldElement(16, 31))
+
+    def test_mul(self):
+        a = FieldElement(24, 31)
+        b = FieldElement(19, 31)
+        self.assertEqual(a * b, FieldElement(22, 31))
+
+    def test_pow(self):
+        a = FieldElement(17, 31)
+        self.assertEqual(a**3, FieldElement(15, 31))
+        a = FieldElement(5, 31)
+        b = FieldElement(18, 31)
+        self.assertEqual(a**5 * b, FieldElement(16, 31))
+
+    def test_div(self):
+        a = FieldElement(3, 31)
+        b = FieldElement(24, 31)
+        self.assertEqual(a / b, FieldElement(4, 31))
+        a = FieldElement(17, 31)
+        self.assertEqual(a**-3, FieldElement(29, 31))
+        a = FieldElement(4, 31)
+        b = FieldElement(11, 31)
+        self.assertEqual(a**-4 * b, FieldElement(13, 31))
+
+
+class Point:
+    def __init__(self, x: Optional[float], y: Optional[float], a: float, b: float):
+        if x is None or y is None:
+            if x != y:
+                raise ValueError(f'x와 y가 둘 중 하나만 None일 수 없습니다.')
+        elif y**2 != x**3 + a * x + b:
+            raise ValueError(f'({x}, {y}) is not on the curve')
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other: object):
+        if not isinstance(other, Point):
+            return False
+        return (
+            self.x == other.x
+            and self.y == other.y
+            and self.a == other.a
+            and self.b == other.b
+        )
+
+    def __repr__(self):
+        return f'Point({self.x}, {self.y}, {self.a}, {self.b})'
+
+    def __add__(self, other: 'Point') -> Self:
+        if isinstance(other, Point):
+            if self.a != other.a or self.b != other.b:
+                raise TypeError(f'Points {self!r}, {other!r} are not on the same curve')
+            if self.is_at_infinite():
+                return other
+            if other.is_at_infinite():
+                return self
+            if self.x == other.x:
+                if self.y != other.y or self.y == other.y == 0:
+                    return self.new_at_infinite()
+                s = (3 * self.x**2 + self.a) / (2 * self.y)
+                x3 = s**2 - 2 * self.x
+            else:
+                s = (other.y - self.y) / (other.x - self.x)
+                x3 = s**2 - self.x - other.x
+            return self.__class__(
+                x3,
+                s * (self.x - x3) - self.y,
+                self.a,
+                self.b,
+            )
+        return NotImplemented
+
+    def new_at_infinite(self) -> Self:
+        return self.__class__(None, None, self.a, self.b)
+
+    def is_at_infinite(self) -> bool:
+        return self.x is None or self.y is None
+
+
+class PointTest(TestCase):
+
+    def test_ne(self):
+        a = Point(x=3, y=-7, a=5, b=7)
+        b = Point(x=18, y=77, a=5, b=7)
+        self.assertTrue(a != b)
+        self.assertFalse(a != a)
+
+    def test_add0(self):
+        a = Point(x=None, y=None, a=5, b=7)
+        b = Point(x=2, y=5, a=5, b=7)
+        c = Point(x=2, y=-5, a=5, b=7)
+        self.assertEqual(a + b, b)
+        self.assertEqual(b + a, b)
+        self.assertEqual(b + c, a)
+
+    def test_add1(self):
+        a = Point(x=3, y=7, a=5, b=7)
+        b = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a + b, Point(x=2, y=-5, a=5, b=7))
+
+    def test_add2(self):
+        a = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a + a, Point(x=18, y=77, a=5, b=7))
